85b672c0428424413e6c562fc005d58d00bea938
Black4Sky <70639308+ilyabudnikx@users.noreply.github.com>
2021-12-14 02:26:22 +0300

    Update BigStepJavaTest.java

--- BSTU/pageobj/src/test/java/BigStepJavaTest.java
+++ BSTU/pageobj/src/test/java/BigStepJavaTest.java
@@ -1,13 +1,17 @@
 import Page.BigStapSellersPage;
 import Page.BigStapTradePage;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import Page.BigStepHomePage;
 import org.testng.annotations.Test;
+
 
 public class BigStepJavaTest {
 
