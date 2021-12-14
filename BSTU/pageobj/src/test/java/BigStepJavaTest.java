2adfe07738e523a647c4e26202a0398f07a3628a
Black4Sky <70639308+ilyabudnikx@users.noreply.github.com>
2021-12-14 03:45:11 +0300

    Update BigStepJavaTest.java

--- BSTU/pageobj/src/test/java/BigStepJavaTest.java
+++ BSTU/pageobj/src/test/java/BigStepJavaTest.java
@@ -3,7 +3,7 @@ import Page.BigStapTradePage;
 import Page.BigStepHomePage;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -22,7 +22,7 @@ public class BigStepJavaTest {
 
     @BeforeMethod(alwaysRun = true)
     public void browserSetup() {
-        driver = new EdgeDriver();
+        driver = new OperaDriver();
         Dimension d = new Dimension(1920, 1080);
         driver.manage().window().setSize(d);
         tradePage = new BigStepHomePage(driver)
